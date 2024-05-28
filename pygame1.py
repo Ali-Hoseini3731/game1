aa5dbf4802bd331f40f8f44bd101dfdfb8527c8d
Ali Hoseini <a.h.computer73@gmail.com>
2024-05-28 07:28:41 +0330

    draw rect and circle

--- pygame1.py
+++ pygame1.py
@@ -1,13 +1,25 @@
-import pygame
+import pygame, sys
 
 pygame.init()
 
-win = pygame.display.set_mode((500, 500),)
+win = pygame.display.set_mode((800, 600), )
 pygame.display.set_caption("first game")
 
-running = True
-while running:
+# colo
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BROWN = (125, 125, 125)
+
+pygame.draw.rect(win, BLUE, ((50, 20), (200, 100)), 0)
+pygame.draw.circle(win, RED, (350, 70), 50, 0)
+
+
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-pygame.quit()
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
