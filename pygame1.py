e1f33e061792d143e21e0c09b2de64c50d2e172d
Ali Hoseini <a.h.computer73@gmail.com>
2024-05-28 07:11:00 +0330

    modify pygame

--- pygame1.py
+++ pygame1.py
@@ -2,15 +2,12 @@ import pygame
 
 pygame.init()
 
-win = pygame.display.set_mode((500, 500))
+win = pygame.display.set_mode((500, 500),)
 pygame.display.set_caption("first game")
 
 running = True
-
 while running:
     for event in pygame.event.get():
-        print(event)
         if event.type == pygame.QUIT:
             running = False
-
 pygame.quit()
